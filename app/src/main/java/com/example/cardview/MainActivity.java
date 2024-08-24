b91f24aa5de388f48729cc36d021eb068101caa6
Didier Pinto <didieresnaki@hotmail.com>
2024-08-24 09:12:28 -0500

    Material CardView

--- app/src/main/java/com/example/cardview/MainActivity.java
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -3,42 +3,34 @@ package com.example.cardview;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private int[] imageIds = {R.drawable.reciclaje, R.drawable.electrico, R.drawable.ahorroEnergia};
-    private String[] cardTitles = {"Eficiendcia", "Energia Electrica", "Recursividad"};
-    private String [] cardViewDescripcion= { "Aprovecha al máximo los recursos que tienes. Por ejemplo, usa las cantidades mínimas necesarias de agua y materiales en la producción y el consumo." +
-            "Adopta tecnologías que reduzcan el consumo de energía y recursos. Por ejemplo, utiliza electrodomésticos con certificación energética de alta eficiencia." +
-            "Siempre que sea posible, elige fuentes de energía renovable como la solar, eólica o hidroeléctrica para tus necesidades eléctricas." +
-            "Instala paneles solares en tu hogar o empresa para reducir la dependencia de la red eléctrica y minimizar la huella de carbono" +
-            "Adopta un enfoque de economía circular en el que los productos se diseñen para ser reutilizados, reparados y reciclados, en lugar de simplemente desechados." +
-            "Considera todo el ciclo de vida de los productos, desde la producción hasta el reciclaje. Elige opciones que reduzcan el impacto ambiental en cada etapa del ciclo de vida."
+    private int[] imageIds = {R.drawable.reciclaje, R.drawable.electrico};
+    private String[] cardTitles = {"Eficiencia", "Energia Electrica", "Recursividad"};
+    private String[] cardViewDescripcion = {
+            "Usa las cantidades mínimas necesarias de agua y materiales en la producción y el consumo.",
+            "Utiliza electrodomésticos con certificación energética de alta eficiencia.",
+            "Siempre que sea posible, elige fuentes de energía renovable",
+            "Instala paneles solares en tu hogar o empresa para reducir la dependencia de la red",
+            "Considera todo el ciclo de vida de los productos, desde la producción hasta el reciclaje"
     };
 
-
-    private int currentndex = 0;
+    private int currentIndex = 0;
     private ImageView imagenCardView;
-    private MaterialTextView cardTitulo;
-    private MaterialTextView descripcion;
+    private TextView cardTitulo;
+    private TextView descripcion;
     private MaterialButton next;
     private MaterialButton back;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         imagenCardView = findViewById(R.id.imageView);
@@ -53,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 accionesBottonSiguiente();
-
             }
         });
 
@@ -61,25 +52,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 accionesBottonAtras();
-
-
             }
         });
-
     }
 
     private void accionesBottonAtras() {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = imageIds.length - 1;
+        }
+        settersInitialValues();
     }
 
     private void accionesBottonSiguiente() {
-
-        currentndex ++;
-        if(currentndex>= imageIds.length){
-            currentndex = 0;
+        currentIndex++;
+        if (currentIndex >= imageIds.length) {
+            currentIndex = 0;
         }
         settersInitialValues();
     }
 
     private void settersInitialValues() {
+        imagenCardView.setImageResource(imageIds[currentIndex]);
+        cardTitulo.setText(cardTitles[currentIndex]);
+        descripcion.setText(cardViewDescripcion[currentIndex]);
     }
 }
